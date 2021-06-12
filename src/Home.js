c064dde343d6a24b9b9c5cf7f4dd37f4c99e547b
harshad0889 <harshadofs@gmail.com>
2021-06-12 14:46:44 +0530

    sec

---
+++ src/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+function Header(){
+    return(
+        <div>
+            <p>
+                header part
+            </p>
+        </div>
+    )
+
+}
+
+
+function Footer(){
+    return(
+        <div>
+            <p>
+                footer section
+            </p>
+        </div>
+    )
+}
+export{Header,Footer};
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Header,Footer} from './Home'
+import {Contact} from './Contact';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Contact/>
   </React.StrictMode>,
   document.getElementById('root')
 );
